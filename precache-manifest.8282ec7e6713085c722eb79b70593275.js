6ab0739572ea21ddb0a07ed0472ce1585fdb1003
steviesocks <stephen.stocking@gmail.com>
2020-06-01 18:11:14 -0400

    Updates

---
+++ precache-manifest.8282ec7e6713085c722eb79b70593275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3d7d84df47c8a30fac5c08c793edd1",
+    "url": "/starwarsrolodex/index.html"
+  },
+  {
+    "revision": "8d1a4c8336d8fb68c13d",
+    "url": "/starwarsrolodex/static/css/main.2db41570.chunk.css"
+  },
+  {
+    "revision": "7e9b29cc49d036102a57",
+    "url": "/starwarsrolodex/static/js/2.dcfaf863.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwarsrolodex/static/js/2.dcfaf863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1a4c8336d8fb68c13d",
+    "url": "/starwarsrolodex/static/js/main.6052aaae.chunk.js"
+  },
+  {
+    "revision": "bc256256de37b930cc8d",
+    "url": "/starwarsrolodex/static/js/runtime-main.86d06aa3.js"
+  }
+]);
